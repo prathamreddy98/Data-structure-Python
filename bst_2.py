235cc001e3a5a360197fafea7bb149b159c50683
prathamreddy98 <prathamreddy98@gmail.com>
2024-06-16 23:06:32 -0400

    Implemented Search method

--- bst_2.py
+++ bst_2.py
@@ -1,5 +1,5 @@
 class BST:
-    def __init__(self, key):
+    def __init__(self, key): # Self represents the object 
         self.key=key
         self.lchild=None
         self.rchild=None
@@ -11,7 +11,7 @@ class BST:
         if self.key==data:
             return #Handling duplicate values
         if data<self.key:
-            if self.lchild: # if there is already left child then this condition is true and we need to recusrsively call insert method
+            if self.lchild: # if there is already left child then this condition is true and we need to recusrsively call insert method if self.child is equivalent to if self.child is Falsw
                 self.lchild.insert(data)
             else:
                 self.lchild=BST(data) # Root node does not have any child so we insert a new node
@@ -21,13 +21,27 @@ class BST:
             else:
                 self.rchild=BST(data)
                 
-
-
-
+    def search(self, data): #Here self is representing the object hence self.key. Self is root object self is root here
+        if self.key==data:
+            print("Node is found")
+            return
+        if data<self.key: # Searching in left sub tree
+            if self.lchild:
+                self.lchild.search(data)
+            else:
+                print("Node not found")
+        else:
+            if self.rchild: # if rchild is present recursively search in right sub tree
+                self.rchild.search(data)
+            else:
+                print("Not found")
+            
 
 
 
 
         
 root=BST(10)
-root.insert(20)
+list1=[20,4,30,4,1,5,6,1]
+for i in list1:
+    root.insert(i)
